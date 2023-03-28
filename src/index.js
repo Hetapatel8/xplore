2ba8d4669a808c798383017d6238d029167fb323
Hetapatel8 <71117343+Hetapatel8@users.noreply.github.com>
2023-03-28 21:53:08 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import 'material-symbols';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
     </BrowserRouter>
 );
